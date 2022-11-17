4e3643423a97c7fcc6907d76dfee67784224b812
Valeriy Seregin <valera-s-38@mail.ru>
2022-11-17 22:01:28 +0300

    create main app

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -69,6 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'task_manager.wsgi.application'
+
+AUTH_USER_MODEL = 'main.User'
 
 
 # Database
